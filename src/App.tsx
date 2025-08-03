9e363fa7caba0b471f281d0be9512c954c942bf9
Madhu Dulam <105490961+madhudulam@users.noreply.github.com>
2025-08-03 11:27:48 +0530

    Fix useNavigate router context error

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster } from "@/components/ui/sonner"
 import { TooltipProvider } from "@/components/ui/tooltip"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { AuthProvider } from "@/hooks/useAuth"
 import routes from "./routes"
 import NotFound from "./pages/NotFound"
 import "./App.css"
@@ -13,19 +12,19 @@ const queryClient = new QueryClient()
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <TooltipProvider>
-          <BrowserRouter>
+      <TooltipProvider>
+        <BrowserRouter>
+          <AuthProvider>
             <Routes>
               {routes.map((route, index) => (
                 <Route key={index} path={route.path} element={route.element} />
               ))}
               <Route path="*" element={<NotFound />} />
             </Routes>
-          </BrowserRouter>
-          <Toaster />
-        </TooltipProvider>
-      </AuthProvider>
+            <Toaster />
+          </AuthProvider>
+        </BrowserRouter>
+      </TooltipProvider>
     </QueryClientProvider>
   )
 }
